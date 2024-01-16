dde65d5766b9a9c39a6ac6261a0e5feb5b460e37
cyenglu <lullusa59.nedv59xn.py@gmail.com>
2024-01-16 11:06:30 -0400

    enumftp - comments

--- enumftp.py
+++ enumftp.py
@@ -2,19 +2,27 @@
 
 import socket
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# conectando ao servidor
+
 tcp.connect(("IP",21))
+
 banner = tcp.recv(1024)
 print(banner)
 
+# enviando usuario
 tcp.send("USER ftp\r\n")
 user = tcp.recv(1024)
 print(user)
 
+# enviando a senha
 tcp.send("PASS ftp\r\n")
 password = tcp.recv(1024)
 print(password)
 
+# enviando comando HELP
 tcp.send("HELP\r\n")
+
+# enviando comando PWD
 tcp.send("PWD\r\n")
 cmd = tcp.recv(2048)
 print(cmd)
